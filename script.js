adc3fc91d7eb6eb4e71b389e204838a2c96aee53
Swaroop Ranjan <swarooprout9848@gmail.com>
2025-05-10 11:17:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+  
+    if (window.scrollY > 100) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+  
